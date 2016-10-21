de1694a828567178513847f85b26bf7a9ad96560
rdubniewski <rdubniewski@gmail.com>
2016-10-21 12:09:58 +0200

    uporzą▲dkowane opisy elementów

---
+++ SterownikOswietlenia/Debug/avrdude.flash.bat
@@ -0,0 +1,1 @@
+ "c:\program files\avrdude\avrdude.exe" -p ATmega32 -c stk200 -P lpt1 -i 50 -U lfuse:w:0xFF:m -U hfuse:w:0x49:m  -U flash:w:"C:\Users\Public\Wymiana\Rafal\Git\AVR\SterownikOswietlenia\Debug\SterownikOswietlenia.hex":i 
